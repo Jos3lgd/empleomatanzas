55fe6f1694e8f107d060421557fa8fd11cc77a00
J053 <132166564+Jos3lgd@users.noreply.github.com>
2025-04-13 16:14:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -146,7 +146,8 @@ async def menu(update: Update, context: CallbackContext):
     )
 
 async def ayuda(update: Update, context: CallbackContext):
-    await update.message.reply_text(
+    logger.info("ℹ️ Ayuda solicitada")
+    mensaje = (
         "*Hola, gracias por utilizar nuestro Bot*\n\n"
         "Puedes utilizar los comandos disponibles en el menú en la parte inferior izquierda o teclearlos:\n\n"
         "📎 /start — Iniciar el bot\n"
